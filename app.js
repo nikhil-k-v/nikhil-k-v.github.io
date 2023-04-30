e111db4a077338d4378b11ce65588f73613268fb
nv <nkvijay08@gmail.com>
2023-04-29 23:28:09 -0400

    fixed anims and added braille overall

--- app.js
+++ app.js
@@ -1,125 +1,73 @@
 
 let active = false;
 function animateMe(el) {
-  const title = el.querySelector(".s-title"); // Get the s-title element inside the box
-  const seeMoreButton = el.querySelector(".see-more"); // Get the see-more button inside the box
-  const divider = el.querySelector(".divider"); // Get the see-more button inside the box
-  
-  if (active && el.classList.contains("active")) {
+  const isActive = el.classList.contains("active");
+  const boxes = document.querySelectorAll(".box");
+
+  // Reset all boxes
+  boxes.forEach((box) => {
+    box.classList.remove("active");
+    box.classList.add("skew");
+  });
+
+  // If the clicked box was active, set active to false
+  if (isActive) {
     active = false;
-
-    gsap.to(seeMoreButton, {
-      duration: 0.5,
-      opacity: 0,
-      display: "none",
-      ease: "power1",
-    });
-
-    gsap.to(divider, {
-      duration: 0.5,
-      opacity: 0,
-      display: "none",
-      ease: "power1",
-    });
-
-    const activeBox = document.querySelectorAll(".active");
-    activeBox.className = "box";
-    gsap.to(".box", {
-      backgroundColor: "var(--dark-accent)",
-      borderColor: "var(--dark-accent)",
-      duration: 1,
-      width: "30vw",
-      ease: "power1",
-    });
-
-    // Scale down the text as the box gets smaller
-    gsap.to(title, {
-      duration: 1,
-      fontSize: "1.5em",
-      ease: "power1",
-      bottom: "6%",
-    });
-
-    const boxes = document.querySelectorAll(".box");
-    Array.from(boxes).forEach(function (box) {
-      box.classList.add("skew");
-      box.classList.remove("active");
-    });
   } else {
-    active = true;
-    const boxes = document.querySelectorAll(".box");
-    Array.from(boxes).forEach(function (box) {
-      box.className = "box";
-      box.classList.add("skew");
-    });
+    // Otherwise, activate the clicked box and set active to true
     el.classList.add("active");
     el.classList.remove("skew");
-
-    // Box gets larger here
-    gsap.to(el, {
-      backgroundColor: "var(--dark-secondary)",
-      borderColor: "var(--dark-secondary)",
-      duration: 1.5,
-      width: "60vw",
-      transition: "background-color 1s ease",
-      ease: "power1",
-    });
-
-    gsap.to(seeMoreButton, {
-      duration: 1.5,
-      opacity: 1,
-      display: "block",
-      ease: "power1",
-    });
-
-    gsap.to(divider, {
-      duration: 1.5,
-      opacity: 0.75,
-      display: "block",
-      ease: "power1",
-    });
-    
-    // Scale up the text as the box gets larger
-    gsap.to(title, {
-      duration: 1.5,
-      fontSize: "2.6em", // Adjust this value to control the scaling
-      ease: "power4",
-      bottom: '12%'
-    });
-
-    gsap.to(".box:not(.active)", {
-      backgroundColor: "var(--dark-accent)",
-      borderColor: "var(--dark-accent)",
-      duration: 1.5,
-      width: "30vw",
-      ease: "power1",
-    });
+    active = true;
   }
+
+  // Animate boxes and text size
+  boxes.forEach((box) => {
+    const boxIsActive = box.classList.contains("active");
+    const boxWidth = boxIsActive ? "60vw" : "30vw";
+    const titleFontSize = boxIsActive ? "3em" : "1.5em";
+    const titleBottom = boxIsActive ? "13%" : "6%";
+    const durationToggle = boxIsActive ? 1 : 0.5;
+    const opacityToggle = boxIsActive ? 1 : 0;
+    const activeToggle = boxIsActive ? "block" : "none";
+    const bgColor = boxIsActive
+      ? "var(--dark-secondary)"
+      : "var(--dark-accent)";
+    const borderColor = boxIsActive
+      ? "var(--dark-secondary)"
+      : "var(--dark-accent)";
+
+    gsap.to(box, {
+      backgroundColor: bgColor,
+      borderColor: borderColor,
+      duration: 1.5,
+      width: boxWidth,
+      ease: "power1",
+    });
+
+    gsap.to(box.querySelector(".s-title"), {
+      duration: 1.5,
+      bottom: titleBottom,
+      fontSize: titleFontSize,
+      ease: "power2",
+    });
+
+    gsap.to(box.querySelector(".divider"), {
+      duration: durationToggle,
+      opacity: opacityToggle,
+      display: activeToggle,
+      ease: "power1",
+    });
+
+    gsap.to(box.querySelector(".see-more"), {
+      duration: durationToggle,
+      opacity: opacityToggle,
+      display: activeToggle,
+      ease: "power1",
+    });
+  });
 }
 
-
-// turn vertical scrolling into horizontal scrolling, everywhere on the page, smoothly
-//slow down the scrolling in this function by adding a multiplier
-
-// document.addEventListener('wheel', function(e) {
-//     if (e.deltaY !== 0) {
-//         window.scrollBy((e.deltaY * 0.4), 0);
-//         //animate the custom scrollbar at the top of the page using the scrollbar, handle, and mousearea elements
-//         gsap.to(".handle", {
-//           duration: 0.5,
-//           x: window.scrollX * 0.7575,
-//           ease: "power2",
-//         });
-//     }
-//     e.preventDefault();}
-// );
-
-// turn vertical scrolling into horizontal scrolling, everywhere on the page, smoothly
-//slow down the scrolling in this function by adding a multiplier
-
-// using draggable, make the handle draggable, and scroll the page horizontally
-// when the handle is dragged
-
+//scrolling
 let isDragging = false;
 let initialPointerPosition = 0;
 let initialHandlePosition = 0;
@@ -156,6 +104,11 @@ function initializeDraggable() {
 
 document.addEventListener('DOMContentLoaded', function() {
   initializeDraggable();
+  const firstBox = document.querySelector(".box");
+  if (firstBox) {
+    animateMe(firstBox);
+    animateMe(firstBox);
+  }
 });
 
 document.addEventListener('wheel', function(e) {
@@ -164,12 +117,12 @@ document.addEventListener('wheel', function(e) {
         //animate the custom scrollbar at the top of the page using the scrollbar, handle, and mousearea elements
         gsap.to(".handle", {
           duration: 0.5,
-          x: window.scrollX * 0.115,
+          x: window.scrollX * 0.12,
           ease: "power2",
         });
     }
-    e.preventDefault();}
-);
+    e.preventDefault();
+});
 
 window.addEventListener('scroll', function() {
   // Animate the custom scrollbar when scrolling horizontally
@@ -180,24 +133,3 @@ window.addEventListener('scroll', function() {
   });
 });
 
-// main.js
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   const themeButtons = document.querySelectorAll(".themeButton");
-
-//   themeButtons.forEach((button) => {
-//       button.addEventListener("click", () => {
-//           const theme = button.getAttribute("data-theme");
-//           document.body.className = theme + "-theme";
-//       });
-//   });
-// });
-
-
-
-
-
-
-
-
-    
